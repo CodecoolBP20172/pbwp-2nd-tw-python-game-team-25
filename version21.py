31196ab14f77e177fbd31ac44ab0d68be81ae4d6
Balint Mogyorosi <bg.mogyorosi@gmail.com>
2017-06-29 19:32:46 +0200

    Whole battleship finished

--- version21.py
+++ version21.py
@@ -20,9 +20,9 @@ def print_message(string):
     sys.stdout.flush()
 
 
-def game_exit():
+def game_exit(string):
     print('\033[1;36m')
-    print_title('You exited from the game!'.center(cols, ' '), "=")
+    print_title('{}'.format(string).center(cols, ' '), "=")
     print('\033[0m')
     exit()
 
@@ -235,6 +235,12 @@ def get_coordinate_from_user(sizes, coordinate):
     return create_error_message('Invalid coordinate!')
 
 
+def get_coordine_from_ai(sizes):
+    x = random.randint(0, sizes[1] - 1)
+    y = random.randint(0, sizes[0] - 1)
+    return [x, y]
+
+
 def create_battleground(sizes):
     battleground = []
     for y in range(sizes[0]):
@@ -250,7 +256,6 @@ def show_battleground(battleground, settings, PLACE_CONSTANTS, own=True):
     cell_width = 5
     ships = settings[5]['value']
     abc = 'ABCDEFGHIJKLOMN'
-    single_line(cols, '-')
     for i in range(-cell_height, len(battleground) * cell_height):
         y = i // cell_height
         center_y = (i % cell_height == cell_height // 2)
@@ -350,58 +355,179 @@ def place_ship_into_battleground(PLACE_CONSTANTS, ships, ship_index, battlegroun
             y += 1
 
 
-def placing_phase(settings, PLACE_CONSTANTS, battleground):
+def placing_phase(player_on_turn, settings, PLACE_CONSTANTS, battleground):
     sizes = settings[4]['value']
     ships = settings[5]['value']
     message = ''
     ships_placed = []
+    if player_on_turn == 'AI':
+        for i in range(5):
+            clear_screen(message)
+            print_title('AI\'s turn', '=')
+            show_battleground(battleground, settings, PLACE_CONSTANTS)
+            single_line(cols, '-')
+            print(
+                'AI is placing ships: {}{}'.format(
+                    ('\033[0;37;40m%{}s\033[0m'.format(
+                        (i + 1) * 2) %
+                        ' '), ('\033[0;37;47m%{}s\033[0m'.format(
+                            (4 - i) * 2) %
+                        ' ')))
+            time.sleep(1)
     while len(ships_placed) < len(ships):
-        # Print out the battleground
-        # Print out the remaining ships
         clear_screen(message)
-        print_title('Player No. {}'.format(1), '=')
-        show_battleground(battleground, settings, PLACE_CONSTANTS)
-        print_title('Ships left to place:', '-')
-        for index in range(len(ships)):
-            if index not in ships_placed:
-                print('{}:{}\t- {} wide'.format((index + 1), ships[index][0], ships[index][1]))
-        single_line(cols, '-')
-        try:
-            ship_index = int(input('Please give the index of a ship: ')) - 1
-        except ValueError:
-            ship_index = -1
-        if ship_index in ships_placed or ship_index < 0 or ship_index > len(ships):
-            message = create_error_message('The ship index should be one from the list below!').format(len(ships))
-            continue
-        single_line(cols, '-')
-        coordinate = []
-        message = get_coordinate_from_user(sizes, coordinate)
-        if message:
-            continue
-        single_line(cols, '-')
-        rotation = input('Rotation: h(orizontal) or v(ertical): ')
-        if rotation not in ['h', 'v']:
-            message = create_error_message('Invalid rotation! It should be \'h\' or \'v\'!').format(len(ships))
-            continue
+        if player_on_turn == 'AI':
+            while True:
+                ship_index = random.randint(0, len(ships) - 1)
+                if ship_index not in ships_placed:
+                    break
+            coordinate = get_coordine_from_ai(sizes)
+            rotation = ['h', 'v'][random.randint(0, 1)]
+        else:
+            print_title('Player No. {}\'s turn'.format(player_on_turn), '=')
+            show_battleground(battleground, settings, PLACE_CONSTANTS)
+            print_title('Ships left to place:', '-')
+            for index in range(len(ships)):
+                if index not in ships_placed:
+                    print('{}:{}\t- {} wide'.format((index + 1), ships[index][0], ships[index][1]))
+            single_line(cols, '-')
+            try:
+                ship_index = int(input('Please give the index of a ship: ')) - 1
+            except ValueError:
+                ship_index = -1
+            if ship_index in ships_placed or ship_index < 0 or ship_index > len(ships):
+                message = create_error_message('The ship index should be one from the list below!').format(len(ships))
+                continue
+            single_line(cols, '-')
+            coordinate = []
+            message = get_coordinate_from_user(sizes, coordinate)
+            if message:
+                continue
+            single_line(cols, '-')
+            rotation = input('Rotation: h(orizontal) or v(ertical): ')
+            if rotation not in ['h', 'v']:
+                message = create_error_message('Invalid rotation! It should be \'h\' or \'v\'!').format(len(ships))
+                continue
         if not ship_can_be_placed(PLACE_CONSTANTS, ships, sizes, ship_index, battleground, coordinate, rotation):
-            message = create_error_message('The ship doesn\'t fit in, or there is an overflow!')
+            if player_on_turn != 'AI':
+                message = create_error_message('The ship doesn\'t fit in, or there is an overflow!')
             continue
         place_ship_into_battleground(PLACE_CONSTANTS, ships, ship_index, battleground, coordinate, rotation)
         ships_placed.append(ship_index)
-        message = create_success_message('The {} has been placed into the battleground!'.format(ships[ship_index][0]))
+        if player_on_turn != 'AI':
+            message = create_success_message('{} has been placed into the battleground!'.format(ships[ship_index][0]))
+
+
+def fighting_phase(player_on_turn, battleground, settings, PLACE_CONSTANTS, ships_hp, ships_left, message):
+    while True:
+        out = ''
+        for i in range(len(message)):
+            out += (message[0] + '\n')
+            message.pop(0)
+        clear_screen(out)
+        if player_on_turn == 'AI':
+            title = 'AI\'s turn'
+        else:
+            title = 'Player No. {}\'s turn'.format(player_on_turn)
+        print_title(title, '-')
+        show_battleground(battleground, settings, PLACE_CONSTANTS, False)
+        if player_on_turn == 'AI':
+            coordinate = get_coordine_from_ai(settings[4]['value'])
+        else:
+            coordinate = []
+            single_line(cols, '-')
+            error = get_coordinate_from_user(settings[4]['value'], coordinate)
+            if error:
+                message.append(error)
+                continue
+        ship_index = battleground[coordinate[1]][coordinate[0]]
+        if ship_index in [PLACE_CONSTANTS['HIT'], PLACE_CONSTANTS['MISSED']]:
+            message.append(create_error_message('This place has been already shot!'))
+            continue
+        if player_on_turn == 'AI':
+            print_title('AI shooting ...', '-')
+            time.sleep(3)
+        if ship_index == 0:
+            battleground[coordinate[1]][coordinate[0]] = PLACE_CONSTANTS['MISSED']
+            message.append(create_error_message('The fire missed!'))
+            return 2
+        ship_index -= PLACE_CONSTANTS['SHIP_START']
+        # The index for decreasing numbers in ships_hp and ships_left
+        if player_on_turn == 1:
+            player_hit = 1
+        else:
+            player_hit = 0
+        ships_hp[ship_index][player_hit] -= 1
+        battleground[coordinate[1]][coordinate[0]] = PLACE_CONSTANTS['HIT']
+        message.append(create_success_message('The fire hit!'))
+        if ships_hp[ship_index][player_hit] == 0:
+            message.append(create_success_message('The {} ship sank!'.format(settings[5]['value'][ship_index][0])))
+            ships_left[player_hit] -= 1
+            message.append(create_success_message('There are {} ships remaining!'.format(ships_left[player_hit])))
+            if ships_left[player_hit] == 0:
+                return 0
+        return 1
+    pass
+
+
+def game_win(player_on_turn, settings, PLACE_CONSTANTS, battleground_won, battleground_lost):
+    clear_screen(create_warning_message('The game has ended!'))
+    if player_on_turn == 'AI':
+        winner = 'AI'
+        loser = 'Player No. 1'
+    else:
+        winner = 'Player No. {}'.format(player_on_turn)
+        if settings[1]['value'] == 1:
+            loser = 'AI'
+        else:
+            loser = 'Player No. {}'.format(player_on_turn % 2 + 1)
+    winner_text = winner + ' won the game!!'
+    print_title(winner_text.center(cols, ' '), '-')
+    # print(winner)
+    # show_battleground(battleground_won, settings, PLACE_CONSTANTS, True)
+    # print(loser)
+    # show_battleground(battleground_lost, settings, PLACE_CONSTANTS, True)
+    game_exit('The game has ended!')
+    pass
 
 
 def start_game(settings, PLACE_CONSTANTS):
-    # Placing phase started
-    # Show the battleground and the ships
-    # Place the ships: ID, coordinate, rotation
-    # Check if the ship can place
-    # Place
+    # Create the battlegrounds
     player1 = create_battleground(settings[4]['value'])
     player2 = create_battleground(settings[4]['value'])
-    placing_phase(settings, PLACE_CONSTANTS, player1)
-    show_battleground(player1, settings, PLACE_CONSTANTS)
+    # Placing phase starts
+    player_on_turn = 1
+    placing_phase(player_on_turn, settings, PLACE_CONSTANTS, player1)
+    if settings[1]['value'] == 1:
+        player_on_turn = 'AI'
+    else:
+        player_on_turn = 2
+    placing_phase(player_on_turn, settings, PLACE_CONSTANTS, player2)
+    # Fighting phase starts
+    ships_hp = []
+    for i in range(len(settings[5]['value'])):
+        ships_hp.append([settings[5]['value'][i][1], settings[5]['value'][i][1]])
+    ships_left = [len(settings[5]['value']), len(settings[5]['value'])]
+    show_battleground(player2, settings, PLACE_CONSTANTS)
     input()
+    player_on_turn = 1
+    message = []
+    while True:
+        if player_on_turn == 1:
+            result = fighting_phase(player_on_turn, player2, settings, PLACE_CONSTANTS, ships_hp, ships_left, message)
+            if result == 2 or (result == 1 and settings[3]['value'] == 1):
+                if settings[1]['value'] == 1:
+                    player_on_turn = 'AI'
+                else:
+                    player_on_turn = 2
+            elif result == 0:
+                game_win(player_on_turn, settings, PLACE_CONSTANTS, player1, player2)
+        else:
+            result = fighting_phase(player_on_turn, player1, settings, PLACE_CONSTANTS, ships_hp, ships_left, message)
+            if result == 2 or (result == 1 and settings[3]['value'] == 1):
+                player_on_turn = 1
+            elif result == 0:
+                game_win(player_on_turn, settings, PLACE_CONSTANTS, player2, player1)
 
 
 def main():
@@ -441,7 +567,7 @@ def main():
             },
             3: {
                 'name': 'game_mode',
-                'value': 1,
+                'value': 2,
                 'function': choose_game_mode,
                 'success': 'The game mode updated successfully!'
             },
@@ -458,13 +584,14 @@ def main():
                 'success': 'The ships updated successfully!'
             }
         }
+        # game_win(1, settings, 'player2', 'player1')
         while True:
             clear_screen(message)
             game_datas(settings)
             action = main_menu()
             message = create_warning_message("You returned back to the main menu!")
             if action == 0:
-                game_exit()
+                raise KeyboardInterrupt
             elif action in list(settings.keys()):
                 message = create_action(action, settings)
             elif action == 6:
@@ -473,7 +600,7 @@ def main():
                 message = create_error_message('The input should be a number between 1-6!')
                 continue
     except KeyboardInterrupt:
-        game_exit()
+        game_exit('You exited the game!')
 
 
 main()
